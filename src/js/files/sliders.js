d6b6cf394d8e54dd7a71ce4023220824260cdb7f
dima23ua <dima23ua@gmail.com>
2022-11-25 00:35:34 +0300

    flowers

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -1,5 +1,5 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+import Swiper, { Navigation, Pagination, EffectFade, Thumbs } from 'swiper';
+Swiper.use([Navigation, Pagination, EffectFade, Thumbs]);
 
 const slider = new Swiper('.header-bottom-nav', {
 	clickable: true,
@@ -16,3 +16,47 @@ const slider = new Swiper('.header-bottom-nav', {
 		},
 	},
 });
+
+const mainSliderLarge = new Swiper('.large-banner-slider', {
+	clickable: true,
+	speed: 1500,
+	slidesPerView: 'auto',
+	direction: 'horizontal',
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+});
+
+const mainSliderSmall = new Swiper('.card-slider', {
+	clickable: true,
+	speed: 1500,
+	slidesPerView: 'auto',
+});
+
+const mainAppearence = new Swiper('.appearence-slider', {
+	clickable: true,
+	speed: 1500,
+	slidesPerView: 'auto',
+	spaceBetween: 12,
+});
+
+const productSlider = new Swiper('.product-slider', {
+	clickable: true,
+	speed: 1500,
+	slidesPerView: 'auto',
+	spaceBetween: 20,
+
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+});
+
+
